f5c7bfdaa0a6c7ee2d8240e577a6023bbc18879f
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-07-21 16:05:08 -0700

    context and portals (modal is not working yet)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
   // 3 parameters for react.createelement,
   // 1st what type of element,
   // 2nd any attributes to the component,
   // 3rd whatever children you want to pass to the element
-
+  const themeHook = useState("peru");
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
