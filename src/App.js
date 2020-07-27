c32bc4715dcb75b9ac28f5d7a8b7a3934e436f3f
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-07-27 11:04:45 -0700

    split code, lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
-import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
 
-const Details = lazy(() => import('./Details'));
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   // 3 parameters for react.createelement,
   // 1st what type of element,
   // 2nd any attributes to the component,
-  // 3rd whatever children you want to pass to the element
+  //   3rd whatever children you want to pass to the element
   const themeHook = useState("peru");
   return (
     <React.StrictMode>
--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { lazy } from "react";
 import pet from "@frontendmasters/pet";
 import { navigate } from "@reach/router";
-import Modal from "./Modal";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import _ from "lodash";
+import moment from "moment";
+
+const Modal = lazy(() => import("./Modal"));
+
+console.log(_, moment);
 
 class Details extends React.Component {
   state = { loading: true, showModal: false };
@@ -17,18 +22,20 @@ class Details extends React.Component {
   //   };
   // }
   componentDidMount() {
-    pet.animal(this.props.id).then(({ animal }) => {
-      this.setState({
-        url: animal.url,
-        name: animal.name,
-        animal: animal.type,
-        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-        description: animal.description,
-        media: animal.photos,
-        breed: animal.breeds.primary,
-        loading: false,
-      });
-    }, console.error);
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          url: animal.url,
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      }, console.error);
   }
 
   toggleModal = () => this.setState({ showModal: !this.state.showModal });
